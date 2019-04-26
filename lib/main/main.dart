31fac0122d780ef4738ce2f6a4dcec383e48c210
benjacobson <bentjacobson@gmail.com>
2019-04-25 22:39:05 -0700

    Update the color scheme.

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -8,13 +8,14 @@ class FlipsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeScreen(),
+      // The color scheme is muted gray/blue with bright popping tile colors.
       theme: ThemeData(
         accentColor: Color.fromRGBO(250, 250, 250, 1.0),
-        backgroundColor: Color.fromRGBO(10, 20, 40, 1.0),
+        backgroundColor: Color.fromRGBO(20, 20, 20, 1.0),
         disabledColor: Color.fromRGBO(150, 150, 150, 1.0),
         hintColor: Color.fromRGBO(249, 215, 126, 1.0),
         pageTransitionsTheme: FlipsPageTransitionsTheme(),
-        primaryColor: Color.fromRGBO(30, 50, 120, 1.0),
+        primaryColor: Colors.blueGrey,
       ),
     );
   }
